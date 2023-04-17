d8a8904292f93139fd912753020584472acb8ee0
Arnon  Ruangthanawes <arnon_r@hotmail.co.nz>
2023-04-17 14:28:01 +0700

    Fix login issue

--- client/src/routes/ProtectedRoute.tsx
+++ client/src/routes/ProtectedRoute.tsx
@@ -1,14 +1,15 @@
 import { Navigate } from 'react-router-dom';
 import appUtils from '../utils/appUtils';
-// import { useStore } from 'zustand';
-// import { useUserStore } from '../stores/UserStores';
+import { useStore } from 'zustand';
+import { useUserStore } from '../stores/UserStores';
 
 export const ProtectedRoute = ({ children }: { children: JSX.Element }) => {
   // To access tokens from store
   // const tokens = useStore(useUserStore, (state) => state.tokens);
+  const isLoggedIn = useStore(useUserStore, (state) => state.isLoggedIn);
 
   const loggedIn = appUtils.getCookie('logged_in');
-  if (loggedIn === 'true') {
+  if (loggedIn === 'true' || isLoggedIn) {
     return children;
   }
   return <Navigate to="/login" />;
